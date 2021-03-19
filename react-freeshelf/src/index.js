c6b8e984d6d9cffca312769eef89d66cea796db5
Albany <albany.prince@gmail.com>
2021-03-18 20:04:12 -0400

    images and info showing

--- react-freeshelf/src/index.js
+++ react-freeshelf/src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import data from './data.js'
+
+console.log(data)
+
+const App = () => {
+  
+  const[books] = useState(data)
+  return (
+    <main>
+      <h1>FREE SHELF</h1>
+      {books.map((book) => (
+        <Book title={book.title} author={book.author} description={book.shortDescription} cover={book.coverImageUrl}/>
+      ))}
+    </main>
+  )
+}
+
+const Book = (props) => {
+  const { title, author, description, cover } = props
+  return (
+    <div className="dev">
+      <img src={cover} alt='cover'></img>
+      <h2>{title}</h2>
+      <p>{author}</p>
+      <p>{description}</p>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+  document.querySelector("#root")
+)
